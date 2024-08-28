5615e4efda1d1ae143517bc02bcb11bf282eebf1
Hieronymbus <birdsallalexander1990@gmail.comm>
2024-08-28 16:11:41 +1200

    MVP

--- src/square.jsx
+++ src/square.jsx
@@ -1,27 +1,39 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
-function Square ({ key }) {
+function Square ({  player, setPlayer, id, setXSquares, setOSquares, handleWin, xSquares, oSquares  }) {
 
-        const [showX, setShowX] = useState(false)
-        const [showO, setShowO] = useState(false)
+    const [showX, setShowX] = useState(false);
+    const [showO, setShowO] = useState(false);
 
+    const handleSquareClick = (e) => {
+        
+        if(showX || showO) {
+            return;
+            
+        } else if(player === "one"){
+            setShowX(true);
+            setPlayer("two");
+            console.log("id = ", e.target.id)
+            setXSquares(xSquares => [...xSquares, e.target.id])
+        } else {
+            setShowO(true);
+            setPlayer("one");
+            console.log("id = ", e.target.id)
+            setOSquares(oSquares => [...oSquares, (e.target.id)
+            ])
+        };
+         
+    };
 
-        const handleSquareClick = () => {
-            if(playerOne) {
-                setShowX(true)
-                setPlayer("One")
-            } else if (playerTwo) {
-                setShowO(true)
-                setPlayer("two")
-            }
-        }
+    
 
-        return (
+    return (
 
-            <div className="square" key={key} onClick={handleSquareClick} >
-                <h1>{showX ? "X" : ""}{showO? "O" : ""}</h1>
-            </div>
-        )
+        <div className="square"  id={id} onClick= {(e) => handleSquareClick(e)}>
+            {showX ? "X" : "" }
+            {showO ? "O" : ""}   
+        </div>
+    )
 }
 
 export default Square
