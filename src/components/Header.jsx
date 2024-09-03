7722669128b6f72ea84ddf34a38378ffc530f3ee
Dave-ctr <davidjamieson1985@gmail.com>
2024-09-03 22:04:38 +1200

    Apply top margin to h1 for mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header({ player, handleReset, handlePlayAgain, gameWon }) {
   return (
     <div className="w-full min-h-40 flex flex-col items-center justify-center md:p-10">
-      <h1 className="text-center text-6xl font-semibold p-2 mb-16 md:mb-8">TIC TAC TOE</h1>
+      <h1 className="text-center text-6xl font-semibold p-2 mt-8 md:mt-0 mb-16 md:mb-8">TIC TAC TOE</h1>
       <div className="flex flex-col md:flex-row gap-10">
         {!gameWon && (
           <h2 className="text-2xl p-2 mb-3 border-2 border-slate-400 rounded-lg bg-gradient-to-tr from-purple-400 via-pink-500 to-red-500">
